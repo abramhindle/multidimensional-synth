45bb583686d80ab5540f14e3bdb1e9c8c1f156fe
Abram Hindle <abram.hindle@ualberta.ca>
2019-07-25 14:34:21 -0600

    Multidimensional surface + update

--- lm.js
+++ lm.js
@@ -1,13 +1,14 @@
 // const LM = require('ml-levenberg-marquardt').default;
 const LM = require('ml-levenberg-marquardt');
 
+var dims = 10;
 // function that receives the parameters and returns
 // a function with the independent variable as a parameter
 function sinFunction([a, b]) {
   return (t) => a * Math.sin(b * t);
 }
-function circle([x,y,z]) {
-    return (t) => Math.abs(x*x + y*y + z*z - 1.0);
+function circle(vec) {
+    return (t) => Math.abs(vec.map( x => x*x ).reduce( (acc,y) => acc + y) - 1.0);
 }
 
 // array of points to fit
@@ -21,17 +22,18 @@ let data = {
 };
 
 // array of initial parameter values
-let initialValues = [
-   1.0, 1.0, 1.0
-];
+let initialValues = new Array(dims).fill(0);
 
 // Optionally, restrict parameters to minimum & maximum values
-let minValues = [
+/* let minValues = [
   -2,-2,-2
-];
-let maxValues = [
+]; */
+let minValues = new Array(dims).fill(-2.0);
+let maxValues = new Array(dims).fill(2.0);
+/* let maxValues = [
   2,2,2
 ];
+*/
 
 const options = {
   damping: 1.5,
@@ -40,13 +42,14 @@ const options = {
   maxValues: maxValues,
   gradientDifference: 10e-2,
   maxIterations: 100,
-  errorTolerance: 10e-3
+  errorTolerance: 10e-4
 };
 
 for (let i = 0 ; i < 1000; i++) {
-	initialValues[0] = Math.random();
-	initialValues[1] = Math.random();
-	initialValues[2] = Math.random();
+	initialValues.forEach( (v,i) => initialValues[i] = Math.random() );
+	// initialValues[0] = Math.random();
+	// initialValues[1] = Math.random();
+	// initialValues[2] = Math.random();
 	let fittedParams = LM(data, circle, options);
 	console.log(fittedParams);
 }
